5b83bc79023e0a10a825e4366b24161b95227e2d
dwargov <arturbakowski@pm.me>
2022-01-16 16:58:48 +0100

    client-side implementation

---
+++ client.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+import datetime
+import websockets
+
+from ocpp.v16 import call
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16.enums import *
+
+logging.basicConfig(level=logging.INFO)
+
+URL = 'ws://localhost:9000/CP_1'
+
+class ChargePoint(cp):
+    async def send_boot_notification(self):
+        request = call.BootNotificationPayload(
+            charge_point_model="Test1",
+            charge_point_vendor="Test2"
+        )
+
+        response = await self.call(request)
+
+async def main():
+    async with websockets.connect(
+        URL,
+        subprotocols=['ocpp1.6']
+    ) as ws:
+        cp = ChargePoint('CP_1', ws)
+        await asyncio.gather(cp.start(), cp.send_boot_notification())
+
+if __name__ == '__main__':
+    asyncio.run(main())
